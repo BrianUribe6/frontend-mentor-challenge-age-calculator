92881ed0f622b8904dfcf7c8117984c9e85d6671
brianuribe6 <brian.uribe@outlook.com>
2023-04-16 12:23:57 -0400

    Refactoring, an Error out on incorrect leap years

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import IconArrow from "./components/IconArrow";
-import Input from "./components/Input";
 import dayjs from "dayjs";
-
-type FormValues = {
-  day: number;
-  month: number;
-  year: number;
-};
+import { useState } from "react";
+import AgeForm, { FormValues } from "./components/AgeForm";
 
 type ResultProps = {
   // Overengineered, lol
   [TName in keyof FormValues as `${TName}s`]?: number;
 };
 
-type FormProps = {
-  onSubmit?(data: FormValues): void;
-};
-
 function App() {
   const [age, setAge] = useState<ResultProps>({});
-
   const handleSubmit = ({ day, month, year }: FormValues) => {
     // Display age in years, months and days since the given date, e.g.
     // between 05/28/1998 and 04/15/2023, the age is 24 years, 10 months and 18 days
     const today = dayjs();
     const birthday = dayjs(`${year}-${month}-${day}`);
+
     const years = today.diff(birthday, "year");
     const months = today.subtract(years, "year").diff(birthday, "month");
-
-    console.log("Today - years", today.subtract(years, "year").toString());
-    console.log(
-      "Today - years - months",
-      today.subtract(years, "year").subtract(months, "month").toString()
-    );
-
     const days = today
       .subtract(years, "year")
       .subtract(months, "month")
@@ -47,91 +28,10 @@ function App() {
   return (
     <main className="min-h-screen bg-offWhite p-4">
       <div className="bg-white mt-20 rounded-3xl rounded-br-[5rem] px-8 py-14 desktop:px-10 max-w-2xl mx-auto">
-        <Form onSubmit={handleSubmit} />
+        <AgeForm onSubmit={handleSubmit} />
         <Result {...age} />
       </div>
     </main>
-  );
-}
-
-function Form({ onSubmit }: FormProps) {
-  const {
-    handleSubmit,
-    register,
-    setError,
-    formState: { errors },
-  } = useForm<FormValues>({
-    mode: "onBlur",
-  });
-
-  const handleValid: Parameters<typeof handleSubmit>[0] = (data) => {
-    onSubmit?.(data);
-  };
-
-  const handleInvalid = () => {
-    // Clear error messages, but display generic error on the first invalid field
-    setError("month", {});
-    setError("year", {});
-    setError("day", { message: "Must be a valid date" });
-  };
-
-  return (
-    <form onSubmit={handleSubmit(handleValid, handleInvalid)} noValidate>
-      <div className="grid grid-cols-3 gap-4 max-w-md">
-        <Input
-          type="number"
-          label="Day"
-          placeholder="DD"
-          error={errors.day}
-          helperText={errors.day?.message}
-          {...register("day", {
-            required: "Day is required",
-            min: { value: 1, message: "Must be a valid day" },
-            max: { value: 31, message: "Must be a valid day" },
-          })}
-        />
-        <Input
-          type="number"
-          label="Month"
-          placeholder="MM"
-          error={errors.month}
-          helperText={errors.month?.message}
-          {...register("month", {
-            required: "Month is required",
-            min: { value: 1, message: "Must be a valid month" },
-            max: { value: 12, message: "Must be a valid month" },
-          })}
-        />
-        <Input
-          type="number"
-          label="Year"
-          placeholder="YYYY"
-          error={errors.year}
-          helperText={errors.year?.message}
-          {...register("year", {
-            required: "Year is required",
-            min: { value: 1, message: "year cannot be negative" },
-            max: {
-              value: new Date().getFullYear() - 1,
-              message: "Must be in the past",
-            },
-          })}
-        />
-      </div>
-      <div className="relative text-center desktop:text-end my-9">
-        <div
-          className="h-[1px] bg-lightGray absolute 
-        left-0 right-0 top-1/2"
-        />
-        <button
-          type="submit"
-          className="bg-purple rounded-full p-5 hover:bg-offBlack relative"
-          aria-label="calculate"
-        >
-          <IconArrow className="text-2xl desktop:stroke-2 desktop:text-4xl stroke-[4]" />
-        </button>
-      </div>
-    </form>
   );
 }
 
---
+++ src/components/AgeForm.tsx
@@ -0,0 +1,113 @@
+import { useEffect } from "react";
+import { RegisterOptions, useForm } from "react-hook-form";
+import IconArrow from "./IconArrow";
+import Input from "./Input";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
+dayjs.extend(customParseFormat);
+
+export type FormValues = {
+  day: number;
+  month: number;
+  year: number;
+};
+
+type FormProps = {
+  onSubmit?(data: FormValues): void;
+};
+
+export default function AgeForm({ onSubmit }: FormProps) {
+  const {
+    handleSubmit,
+    register,
+    setFocus,
+    formState: { errors },
+  } = useForm<FormValues>({
+    mode: "onBlur",
+  });
+
+  const handleValidSubmit = (data: FormValues) => {
+    onSubmit?.(data);
+  };
+
+  useEffect(() => setFocus("day"), []);
+
+  return (
+    <form onSubmit={handleSubmit(handleValidSubmit)} noValidate>
+      <div className="grid grid-cols-3 gap-4 max-w-md">
+        <Input
+          type="number"
+          label="Day"
+          placeholder="DD"
+          error={errors.day}
+          helperText={errors.day?.message}
+          {...register("day", {
+            required: "Day is required",
+            min: { value: 1, message: "Must be a valid day" },
+            max: { value: 31, message: "Must be a valid day" },
+            validate: {
+              isValidDate(_, { day, month, year }) {
+                if (!day || !month || !year) return true;
+                const date = dayjs(
+                  `${year}-${month}-${day}`,
+                  "YYYY-MM-DD",
+                  true
+                );
+
+                return date.isValid() || "Must be a valid date";
+              },
+            },
+          })}
+        />
+        <Input
+          type="number"
+          label="Month"
+          placeholder="MM"
+          error={errors.month}
+          helperText={errors.month?.message}
+          {...register("month", {
+            required: "Month is required",
+            min: { value: 1, message: "Must be a valid month" },
+            max: { value: 12, message: "Must be a valid month" },
+            deps: ["day", "year"],
+          })}
+        />
+        <Input
+          type="number"
+          label="Year"
+          placeholder="YYYY"
+          error={errors.year}
+          helperText={errors.year?.message}
+          {...register("year", {
+            required: "Year is required",
+            min: { value: 1, message: "Must be a valid year" },
+            validate: {
+              isPast(_, { day, month, year }) {
+                if (!day || !month || !year) return true;
+
+                const date = dayjs(`${year}-${month}-${day}`);
+                const today = dayjs();
+                return date.isBefore(today) || "Must be in the past";
+              },
+            },
+            deps: ["day"],
+          })}
+        />
+      </div>
+      <div className="relative text-center desktop:text-end my-9">
+        <div
+          className="h-[1px] bg-lightGray absolute 
+          left-0 right-0 top-1/2"
+        />
+        <button
+          type="submit"
+          className="bg-purple rounded-full p-5 hover:bg-offBlack relative"
+          aria-label="calculate"
+        >
+          <IconArrow className="text-2xl desktop:stroke-2 desktop:text-4xl stroke-[4]" />
+        </button>
+      </div>
+    </form>
+  );
+}
