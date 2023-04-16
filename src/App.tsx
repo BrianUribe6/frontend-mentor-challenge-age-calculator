a80847f7a800d3c79627ad6c1aac3581e105c4b1
brianuribe6 <brian.uribe@outlook.com>
2023-04-16 14:03:50 -0400

    Added animated counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AgeForm, { FormValues } from "./components/AgeForm";
 
 type ResultProps = {
   // Overengineered, lol
   [TName in keyof FormValues as `${TName}s`]?: number;
+};
+
+type AnimatedCounterProps = {
+  duration?: number;
+  value: number;
 };
 
 function App() {
@@ -41,19 +46,38 @@ function Result({ days, months, years }: ResultProps) {
     { value: months, label: "months" },
     { value: days, label: "days" },
   ];
-
   return (
     <div className="space-y-3">
       {results.map(({ value, label }) => (
         <p
           className="text-5xl desktop:text-6xl font-extrabold italic"
-          key={label}
+          key={`${value}-${label}`}
         >
-          <span className="text-purple">{value ?? "--"}</span> {label}
+          <span className="text-purple">
+            {Number.isFinite(value) ? (
+              <AnimatedCounter value={value!} duration={200} />
+            ) : (
+              "--"
+            )}
+          </span>{" "}
+          {label}
         </p>
       ))}
     </div>
   );
+}
+
+function AnimatedCounter({ value, duration = 1000 }: AnimatedCounterProps) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => Math.min(prevCount + 1, value));
+      if (count === value) clearInterval(interval);
+    }, duration / value);
+    return () => clearInterval(interval);
+  }, []);
+  return <>{count}</>;
 }
 
 export default App;
